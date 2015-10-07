d687f54b388428b8463aeaf178051893fb0ccbf6
way0750 <bruce0750@gmail.com>
2015-10-07 12:06:54 -0700

    stack all done

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,28 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  // length: which is the amount of the items in stack, can be use as current available spot to store incoming value;
+  var length = 0;
+    
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance[length]=value;
+    return length++;
   };
 
   someInstance.pop = function() {
+    
+    if (length>0){
+      length--;
+      var poppedVal = someInstance[length];
+      delete someInstance[length];
+      return poppedVal;
+    }
+
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
