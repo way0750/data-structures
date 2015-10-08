e9a67325e08877944b790fec09f3083da0f346cd
way0750 <bruce0750@gmail.com>
2015-10-07 21:26:27 -0700

    remove edge works

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -29,49 +29,46 @@ each node {
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
-	this.availableID = 0;
 	this.nodes = {};
-
-
 };
 
-var node = function (value) {
+var node = function (valueAsID) {
 	// add a property 'value' to the new node and make sure it is unenumerable;
 	Object.defineProperty(this, "value", {
-		value : value
+		value : valueAsID
 	});
-
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(value) {
+Graph.prototype.addNode = function(valueAsID) {
 	// make new node
-	var newNodeID = this.availableID++;
-	var newNode = new node(value);
-
+	// var newNodeID = this.availableID++;
+	var newNode = new node(valueAsID);
+	
 	// add new node to graph with unique id
-	this.nodes[newNodeID] = newNode;
+	this.nodes[valueAsID] = newNode;
 
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(value, nameInstead) {
+Graph.prototype.contains = function(valueAsID, nameInstead) {
 	// get a list of all the ids in this.nodes
 	// go through them, and access the actual node on it
 	// check that node.value
 	// 	compare === value;
 	// 	
-	var allNodeIDs = Object.keys(this.nodes);
+	// var allNodeIDs = Object.keys(this.nodes);
 
-	for (var i = 0; i < allNodeIDs.length; i++){
-		var nodeID = allNodeIDs[i];
-		if (this.nodes[nodeID].value === value){
-			return nameInstead ? nodeID : true;
-		}
-	}
-	return false;
+	// for (var i = 0; i < allNodeIDs.length; i++){
+	// 	var nodeID = allNodeIDs[i];
+	// 	if (this.nodes[nodeID].value === value){
+	// 		return nameInstead ? nodeID : true;
+	// 	}
+	// }
+	// return false;
+	return nameInstead ? valueAsID : !!this.nodes[valueAsID];
 };
 
 // Graph.prototype.traverse = function (test) {
@@ -93,34 +90,41 @@ Graph.prototype.contains = function(value, nameInstead) {
 
 // ------------------------
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(value) {
+Graph.prototype.removeNode = function(valueAsID) {
 	// update all of the to-be-deleted node's connection
 	// then delete the node;
-	var nodeID = this.contains(value, true);
-	if (nodeID){
-		// go through all of the connection and update about the face the this current node is about to be deleted
-		var connectedNodesID = Object.keys(this.nodes[nodeID]);
-		connectedNodesID.reduce(function(graph, connectionID){
-			delete graph[connectionID][nodeID];
-		}, this);
-		delete this.nodes[nodeID];
-	}
+	// var nodeID = this.contains(value, true);
+	// if (nodeID){
+	// 	// go through all of the connection and update about the face the this current node is about to be deleted
+	// 	var connectedNodesID = Object.keys(this.nodes[nodeID]);
+	// 	connectedNodesID.reduce(function(graph, connectionID){
+	// 		delete graph[connectionID][nodeID];
+	// 	}, this);
+	// 	delete this.nodes[nodeID];
+	// }
+
+	delete this.nodes[valueAsID];
 
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+	return !!this.nodes[fromNode][toNode];
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+	this.nodes[fromNode][toNode] = true;
+	this.nodes[toNode][fromNode] = true;
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+	delete this.nodes[fromNode][toNode];
+	delete this.nodes[toNode][fromNode];	
 };
 
 // ------------------------
