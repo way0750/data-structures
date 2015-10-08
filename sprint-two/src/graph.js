0db1cf67ec8e03561136e31433911604b29c4553
way0750 <bruce0750@gmail.com>
2015-10-07 21:28:26 -0700

    all works

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -54,54 +54,11 @@ Graph.prototype.addNode = function(valueAsID) {
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(valueAsID, nameInstead) {
-	// get a list of all the ids in this.nodes
-	// go through them, and access the actual node on it
-	// check that node.value
-	// 	compare === value;
-	// 	
-	// var allNodeIDs = Object.keys(this.nodes);
 
-	// for (var i = 0; i < allNodeIDs.length; i++){
-	// 	var nodeID = allNodeIDs[i];
-	// 	if (this.nodes[nodeID].value === value){
-	// 		return nameInstead ? nodeID : true;
-	// 	}
-	// }
-	// return false;
-	return nameInstead ? valueAsID : !!this.nodes[valueAsID];
+	return !!this.nodes[valueAsID];
 };
 
-// Graph.prototype.traverse = function (test) {
-// 	// test can be a value or a function for doing more complex comparison
-// 	var callBack;
-// 	if (arguments.length){
-// 		callBack = typeof(test) === 'string' ? function(node){return node.value === test} : test ;
-// 	}
-
-// 	var allNodeIDs = Object.keys(this.nodes);
-// 	for (var i =0; i < allNodeIDs.length; i++){
-// 		var nodeID = allNodeIDs[i];
-// 		if (callBack(this.nodes[nodeID])){
-// 			return true;
-// 		}
-// 	}
-
-// };
-
-// ------------------------
-// Removes a node from the graph.
 Graph.prototype.removeNode = function(valueAsID) {
-	// update all of the to-be-deleted node's connection
-	// then delete the node;
-	// var nodeID = this.contains(value, true);
-	// if (nodeID){
-	// 	// go through all of the connection and update about the face the this current node is about to be deleted
-	// 	var connectedNodesID = Object.keys(this.nodes[nodeID]);
-	// 	connectedNodesID.reduce(function(graph, connectionID){
-	// 		delete graph[connectionID][nodeID];
-	// 	}, this);
-	// 	delete this.nodes[nodeID];
-	// }
 
 	delete this.nodes[valueAsID];
 
@@ -130,6 +87,9 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+	Object.keys(this.nodes).forEach(function(node){
+		cb(node);
+	});
 };
 
 /*
