df753b093ca405300d8a9ac107dcb1c3941718f0
way0750 <bruce0750@gmail.com>
2015-10-07 17:14:35 -0700

    done with linkedlist and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,40 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  // find list.tail, add node by creating a new node obj
+  // update list.tail to the just added node
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (list.head === null){
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
+  // find list.head, remove it
+  // update list.head
   list.removeHead = function() {
+    if (list.head !== null){
+      var formerHead = list.head;
+      list.head = list.head.next;
+      return formerHead.value;
+    }
   };
 
+  // go through the list one node at the time
+  // compare node.value with target
+  // return if found
   list.contains = function(target) {
+    var curNode = list.head;
+    
+    while(curNode !== null){
+      if (curNode.value === target){return true;}
+      curNode = curNode.next;
+    }
+    return false;
   };
 
   return list;
